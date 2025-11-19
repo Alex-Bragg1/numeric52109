1e569595fc8cc459200d2b9e2de2485513cfb9c1
Alex-Bragg1 <anjb20@bath.ac.uk>
2025-11-19 17:13:44 +0000

    testing

--- test_sp.py
+++ test_sp.py
@@ -15,3 +15,37 @@ if __name__ == '__main__':
 
     ## Now do the same for the function in sp
     print(f"The product of {a} and {b} is {op.multiply(a,b)}")
+    
+    
+    
+    # Testing the calculator app 
+    print("\n--- Starting Calculator Test (Type 'exit' to quit) ---")
+    op.run_calculator()
+    print("--- Calculator Test Finished ---")
+
+    ### Test cases for statistics.py and graphics.py ### 
+    import simple_package.statistics as sp_stats
+    import simple_package.graphics as sp_graphics
+    import numpy as np
+
+    # Sample data
+    test_data = np.random.normal(size= 1000)
+
+    try:
+        # 1. Calculate stats and display results (Requirement 1 & 2)
+        results = sp_stats.calculate_stats(test_data)
+        
+        # 2. Plot the results (Requirement 3)
+        sp_graphics.plot_histogram_with_stats(
+            results['data'], 
+            results['mean'], 
+            results['median']
+        )
+
+    except (RuntimeError, TypeError, ValueError) as e:
+        print(f"\nFATAL ERROR: Failed to run statistics and graphics: {e}")
+    
+    
+    
+    
+    
